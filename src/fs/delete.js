b2b8ef63e679f22692ade7ceb751ea0cf62cb3c3
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2024-01-28 19:35:41 +0300

    feat: add delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const dir = dirname(fileURLToPath(import.meta.url));
+const message = 'FS operation failed';
+const file = path.join(dir, 'files', 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+    fs.access(file, (err) => {
+        if (err) {
+            throw new Error(message)
+        } else {
+            fs.unlink(file, () => {})
+        };
+    })
 };
 
 await remove();
