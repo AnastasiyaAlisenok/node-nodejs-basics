cf7aa37cc0c6f2a33b77b1d183b0d29fc95371c6
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2024-01-28 20:21:07 +0300

    feat: add parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+    for (let i = 0; i<process.argv.length; i += 2) {
+        const name = process.argv[i].replace(/--/, '');
+        const value = process.argv[i + 1];
+        console.log(`${name} is ${value}`);
+    }
 };
 
 parseArgs();
