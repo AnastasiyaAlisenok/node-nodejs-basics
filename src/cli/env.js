ea9d4c8e3e59e1b3f8d7e280f43b1e8496df9c35
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2024-01-28 20:09:04 +0300

    feat: add parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    for (let variable in process.env) {
+      if (variable.startsWith('RSS_')) {
+        const value = process.env[variable];
+        console.log(`${variable}=${value}`)
+      }
+    }
 };
 
 parseEnv();
