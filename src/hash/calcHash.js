e4a9b7c22f9b4ebc75f8c90332a6537d6e8355e6
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2024-01-28 20:48:29 +0300

    feat: add hash calculate function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import crypto from 'crypto';
+import url from 'url';
+import path, { dirname } from 'path';
+
+const dir = dirname(url.fileURLToPath(import.meta.url));
+
+const fileToHash = path.join(dir, 'files', 'fileToCalculateHashFor.txt');
+const hash = crypto.createHash('sha256');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const input = fs.createReadStream(fileToHash);
+  input.on('readable', () => {
+  const data = input.read();
+  if (data) {
+    hash.update(data);
+  } else {
+    const fileHash = hash.digest('hex');
+    console.log(fileHash);
+  }
+  });
 };
 
 await calculateHash();
